cc50cd155e2174d978d2a749da79b7bd4fe11813
Yurii <oniks9821@gmail.com>
2025-07-02 17:42:49 +0300

    feat(navigation): create navigation menu

--- src/app/components/header/navigation.tsx
+++ src/app/components/header/navigation.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
-import NavigationItem from './navigation-item';
+import Link from 'next/link';
 
 export interface NavigationProps {
   className?: string;
 }
 
+interface NavigationItemType {
+  href: string;
+  label: string;
+}
+
 export default function Navigation({ className }: NavigationProps) {
+  const items: NavigationItemType[] = [
+    { href: '/', label: 'Home' },
+    { href: '/teachers', label: 'Teachers' },
+    { href: '/favorites', label: 'Favorites' },
+  ];
+
   return (
     <nav className={className}>
-      <ul className="grid w-max grid-flow-col items-center gap-[28px]">
-        <NavigationItem pathname="/">Home</NavigationItem>
-        <NavigationItem pathname="/teachers">Teachers</NavigationItem>
-        <NavigationItem pathname="/favorites">Favorites</NavigationItem>
+      <ul className="grid grid-flow-col items-center gap-[28px]">
+        {items.map((item) => (
+          <li key={item.href}>
+            <Link href={item.href}>
+              <span className="text-dark hover:text-yellow text-base/5 font-normal transition-colors">
+                {item.label}
+              </span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
