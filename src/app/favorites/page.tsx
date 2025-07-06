55eef02167dde8a3305aa794a458ce987c25d33f
Yurii <oniks9821@gmail.com>
2025-07-06 09:37:39 +0300

    change(structure): files transferred

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-
-export interface PageProps {
-}
-
-export default function Page({}: PageProps) {
-  return (
-    <div>Favorites</div>
-  );
-}
+// import React from 'react';
+//
+// import FilterPanel from '@/app/components/ui/filter-panel';
+// import TeacherCard from '@/app/components/ui/teacher-card';
+//
+// export interface PageProps {}
+//
+// export default function Page({}: PageProps) {
+//   return (
+//     <>
+//       <FilterPanel />
+//       <TeacherCard />
+//       <div>Favorites</div>
+//     </>
+//   );
+// }
