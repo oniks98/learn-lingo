27d180edf3d18bc1c06dc4c641af8feb15e44011
Yurii <oniks9821@gmail.com>
2025-07-06 09:35:37 +0300

    change(structure): remove avatar.tsx

---
+++ src/app/@modal/signup/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export interface PageProps {}
+
+export default function Page({}: PageProps) {
+    return (
+        <main >
+            <h1>signup modal</h1>
+        </main>
+    );
+}
