1310a1ed9842bf86bf4b25ffbd1e3b4723dd3c92
Yurii <oniks9821@gmail.com>
2025-07-10 22:37:02 +0300

    refactor(exit): change logic

---
+++ src/lib/icons/exit.tsx
@@ -0,0 +1,15 @@
+const ExitIcon = ({ className }: { className?: string }) => (
+  <svg
+    className={className}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 32 32"
+    fill="none"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth={2.5}
+  >
+    <path d="M24 8L8 24M8 8l16 16" />
+  </svg>
+);
+export default ExitIcon;
