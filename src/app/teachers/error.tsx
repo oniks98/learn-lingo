dc63a13f0ca1c1c9060abd249f8d36044def419a
Yurii <oniks9821@gmail.com>
2025-07-11 22:26:51 +0300

    feat(error): create error handling logic for teachers page

---
+++ src/app/teachers/error.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import Button from '@/app/components/ui/button';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <div className="mx-auto max-w-46 text-center">
+      <h2 className="font-bold text-red-600">Something went wrong!</h2>
+      <pre>{error.message}</pre>
+      {error.digest && <p>Error code: {error.digest}</p>}
+      <Button onClick={() => reset()}>Try again</Button>
+    </div>
+  );
+}
