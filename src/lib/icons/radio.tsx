6089292f6631ee398ad8aadb26c0a1a66aab10a9
Yurii <oniks9821@gmail.com>
2025-07-11 22:29:08 +0300

    feat(radio): create radio-icon for booking-modal

---
+++ src/lib/icons/radio.tsx
@@ -0,0 +1,25 @@
+const RadioButtonIcon = ({ selected }: { selected: boolean }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={20}
+    height={20}
+    viewBox="0 0 20 20"
+    fill="none"
+    className={selected ? 'text-yellow' : 'text-gray-muted'}
+  >
+    {/* Outer circle */}
+    <rect
+      x="1"
+      y="1"
+      width="18"
+      height="18"
+      rx="9"
+      stroke="currentColor"
+      strokeWidth="2"
+    />
+    {selected && (
+      <rect x="5" y="5" width="10" height="10" rx="5" fill="currentColor" />
+    )}
+  </svg>
+);
+export default RadioButtonIcon;
