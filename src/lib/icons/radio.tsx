ca9dfe8d87903b59bbed9cecc55f514ef49d71d0
Yurii <oniks9821@gmail.com>
2025-07-12 10:17:52 +0300

    refactor(radio): change styles icon

--- src/lib/icons/radio.tsx
+++ src/lib/icons/radio.tsx
@@ -1,11 +1,11 @@
-const RadioButtonIcon = ({ selected }: { selected: boolean }) => (
+const RadioButtonIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={20}
     height={20}
     viewBox="0 0 20 20"
     fill="none"
-    className={selected ? 'text-yellow' : 'text-gray-muted'}
+    className="text-yellow"
   >
     <rect
       x="1"
@@ -16,9 +16,8 @@ const RadioButtonIcon = ({ selected }: { selected: boolean }) => (
       stroke="currentColor"
       strokeWidth="2"
     />
-    {selected && (
-      <rect x="5" y="5" width="10" height="10" rx="5" fill="currentColor" />
-    )}
+
+    <rect x="5" y="5" width="10" height="10" rx="5" fill="currentColor" />
   </svg>
 );
 export default RadioButtonIcon;
