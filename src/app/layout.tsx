779d5f276245f5f85286fec3a5c7125bf7ad748f
Yurii <oniks9821@gmail.com>
2025-07-12 10:16:19 +0300

    feat(layout): add sonner for toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
-import { Roboto  } from 'next/font/google';
+import { Roboto } from 'next/font/google';
 import Providers from '@/providers/providers';
-import Header from "@/app/components/header/header"
+import Header from '@/app/components/header/header';
 import '@/styles/globals.css';
+import { Toaster } from 'sonner';
 
-const font = Roboto ({ subsets: ['latin'],
-    weight: ['400', '500', '700'] });
+const font = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] });
 
 export default function RootLayout({
-  children,modal,
+  children,
+  modal,
 }: {
   children: React.ReactNode;
   modal: React.ReactNode;
 }) {
   return (
     <html lang="en" className={font.className}>
-      <body >
-        <Providers><Header/>{children}{modal}</Providers>
+      <body>
+        <Providers>
+          <Header />
+          {children}
+          {modal}
+          <Toaster position="top-center" richColors />
+        </Providers>
       </body>
     </html>
   );
