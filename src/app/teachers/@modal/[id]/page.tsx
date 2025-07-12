c197ebfd003a1022b5fe4a8dbbad5bbd779a1ceb
Yurii <oniks9821@gmail.com>
2025-07-12 21:57:09 +0300

    feat(page-modal): add logic not-found

--- src/app/teachers/@modal/[id]/page.tsx
+++ src/app/teachers/@modal/[id]/page.tsx
@@ -1,14 +1,16 @@
+// переробити на сервер для not found
 'use client';
 
 import { useRouter, useParams } from 'next/navigation';
 import BookingFormModal from '@/app/components/modal/booking-form-modal';
+import { notFound } from 'next/navigation';
 
 export default function BookingModalRoute() {
   const router = useRouter();
   const params = useParams();
   const teacherId = params.id as string;
 
-  if (!teacherId) return null;
+  if (!teacherId) return notFound();
 
   return (
     <BookingFormModal
