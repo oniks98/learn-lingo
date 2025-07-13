9bc2e3a5a5a2341722ba7f0ea58c55347e91960f
Yurii <oniks9821@gmail.com>
2025-07-13 14:06:14 +0300

    refactor(providers): remove global provider pattern

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,33 +1,22 @@
-"use client"
+'use client';
 
 import React, { useState } from 'react';
-import {
-    QueryClient,
-    QueryClientProvider,
-    HydrationBoundary,
-    DehydratedState,
-} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AuthProvider } from '@/contexts/auth-context';
 
 interface ProvidersProps {
-    children: React.ReactNode;
-    dehydratedState?: DehydratedState;
+  children: React.ReactNode;
 }
 
-export default function Providers({ children, dehydratedState }: ProvidersProps) {
-
-    const [queryClient] = useState(() => new QueryClient());
-// NOTE: (Global Provider Pattern - обгортка з HydrationBoundary, page.tsx: тепер у <Providers dehydratedState={...}>, а не тільки в HydrationBoundary
-// NOTE:Per-Route Pattern- убрати Providers і тільки обгортати page.tsx у HydrationBoundary )
-    return (
-        <QueryClientProvider client={queryClient}>
-            <HydrationBoundary state={dehydratedState}>
-                <AuthProvider>
-                {children}
-                </AuthProvider>
-            </HydrationBoundary>
-            <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-    );
+export default function Providers({ children }: ProvidersProps) {
+  const [queryClient] = useState(() => new QueryClient());
+  // NOTE: (Global Provider Pattern - обгортка з HydrationBoundary, page.tsx: тепер у <Providers dehydratedState={...}>, а не тільки в HydrationBoundary
+  // NOTE:Per-Route Pattern- убрати Providers і тільки обгортати page.tsx у HydrationBoundary )
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>{children}</AuthProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
