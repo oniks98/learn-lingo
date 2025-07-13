5fc4c924371e4673eafdaa31aa27b052da17e672
Yurii <oniks9821@gmail.com>
2025-07-13 20:56:35 +0300

    chore(validation): change min1 to min6 signup

--- src/lib/validation/signup.ts
+++ src/lib/validation/signup.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const signUpSchema = z.object({
   name: z.string().min(1, 'Full Name is required'),
   email: z.email({ message: 'Invalid email format' }),
-  password: z.string().min(1, 'Please select a password'),
+  password: z.string().min(6, 'Please select a password min 6'),
 });
 
 export type SignUpFormValues = z.infer<typeof signUpSchema>;
